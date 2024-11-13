dc9b4d089a59803c31bab2b050148c383c8359f4
bibektan <bibektan@gmail.com>
2024-11-13 16:59:07 +0200

    login, register with crypt password is done but jwt authentication remaining

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import"./Login.css"
 import { BiSolidCoffee } from "react-icons/bi";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loginUser = async (e) => {
+        e.preventDefault();
+        if(email === '' || password === '') {
+            alert('All fields are required');
+            return;
+        }
+        try{
+            const response = await axios.post('http://localhost:3001/user/login',
+            {
+                users_email: email,
+                users_password: password
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            console.log('response data from login: ', response.data);
+        }catch(error){
+            console.log('error while login: ', error);
+        }
+    }
+
   return (
     <div className='Container'>
         <div className='app-title'>
@@ -13,13 +41,13 @@ function Login() {
         </div>
         <form className='Form'>
             <label className='Label'>Email</label>
-            <input className='form-input' type='email' placeholder='Enter Email'/>
+            <input onChange={ (e) => setEmail(e.target.value) } className='form-input' type='email' placeholder='Enter Email'/>
             <label className='Label'>Password</label>
-            <input className='form-input' type='password' placeholder='Enter Password' />
+            <input onChange={ (e) => setPassword(e.target.value) } className='form-input' type='password' placeholder='Enter Password' />
             <p id='forgot-password'>
                 <Link to="../ResetPassword">Forgot password?</Link>
             </p>
-            <button className='btn' type='submit' >Login</button>
+            <button onClick={loginUser} className='btn' type='submit' >Login</button>
             <p>No Account? 
                 <Link to={"/register"}>Register</Link>  
             </p>
