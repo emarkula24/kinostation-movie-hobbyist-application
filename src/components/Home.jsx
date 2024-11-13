43753bed34e8dc7f91de117c9ff214a616f71f5a
toshi-Devs <trahim87mustapha@gmail.com>
2024-11-13 14:14:23 +0200

    Header changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import "./Home.css"
-import Sidebar from './Sidebar'
 import Header from './Header'
-// import Categories from './Categories'
+import Categories from './Categories'
 
 function Home() {
   return (
     <div className='home'>
-        <Sidebar />
+        
         <Header />
 
-        {/* <Categories /> */}
+        <Categories />
     </div>
   )
 }
