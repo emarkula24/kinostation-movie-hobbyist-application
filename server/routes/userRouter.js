eb87b75d4320f2b29b0f184b20c00a3ea8e0ed55
bibektan <bibektan@gmail.com>
2024-11-13 16:49:33 +0200

    login, register with crypt password is done but jwt authentication remaining

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,26 @@
 import { pool } from "../helpers/db.js";
 import { Router } from "express";
+import { compare, hash } from 'bcrypt';
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
+// generate token
+
+let refreshTokens = [];
+
+const generateToken = (user)=>{
+    return jwt.sign({ users_id: user.users_id, users_email: user.users_email }, process.env.JWT_SECRET_KEY, {expiresIn: '1h'});
+}
+
+const generateRefreshToken = (user)=>{
+    return jwt.sign({ users_id: user.users_id, users_email: user.users_email }, process.env.JWT_REFRESH_SECRET_KEY);
+}
+
 // Register Route
 router.post("/register", async (req, res, next) => {
+    console.log('hello from register route');
+    console.log(req.body);
     try {
         const { users_email, users_password } = req.body;
 
@@ -12,9 +28,11 @@ router.post("/register", async (req, res, next) => {
             return res.status(400).json({ error: "Invalid email or password." });
         }
 
+        const hashedPassword = await hash(users_password, 10);
+
         const result = await pool.query(
             "INSERT INTO users (users_email, users_password) VALUES ($1, $2) RETURNING *",
-            [users_email, users_password]
+            [users_email, hashedPassword]
         );
         const user = result.rows[0];
         res.status(201).json({
@@ -37,28 +55,54 @@ router.post("/login", async (req, res, next) => {
             return res.status(400).json({ error: "Email and password are required." });
         }
 
+        
         const result = await pool.query(
             "SELECT * FROM users WHERE users_email = $1",
             [users_email]
         );
-
+        
         if (result.rowCount === 0) {
             return res.status(401).json({ error: invalid_message });
         }
 
+        
         const user = result.rows[0];
 
-        if (users_password !== user.users_password) {
-            return res.status(401).json({ error: invalid_message });
-        }
+        if (!await compare(users_password, user.users_password)) return next(new ApiError(invalid_message, 401))
+
+        // Generate tokens
+        const accessToken = generateToken(user);
+        const refreshToken = generateRefreshToken(user);
+
+        refreshTokens.push(refreshToken);
 
         res.status(200).json({
             users_id: user.users_id,
-            users_email: user.users_email
+            users_email: user.users_email,
+            accessToken,
+            refreshToken
         });
     } catch (error) {
         return next(error);
     }
 });
+
+// const verify = (req, res, next) => {
+//     const authHeader = req.headers.authorization;
+//     if (authHeader) {
+//         const token = authHeader.split(" ")[1];
+
+//         jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+//             if (err) {
+//                 return res.status(403).json("Token is not valid!");
+//             }
+
+//             req.user = user;
+//             next();
+//         });
+//     } else {
+//         res.status(401).json("You are not authenticated!");
+//     }
+// };
 
 export default router;
