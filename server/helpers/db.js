cc25c999399ac3df3b5db5e816697bbe009563e5
emarkula24 <emarkula24@student.oulu.fi>
2024-11-26 20:04:29 +0200

    Configured test enviroment

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -3,15 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log(`Connecting to database: ${process.env.NODE_ENV === "development" ? process.env.DB_NAME : process.env.TEST_DB_NAME}`);
+console.log(`Database Host: ${process.env.DB_HOST}`);
+console.log(`Database Port: ${process.env.NODE_ENV === "development" ? process.env.DB_PORT : process.env.TEST_DB_PORT}`);
+
+
 const { Pool } = pkg;
 
 const openDb = () => {
     const pool = new Pool ({
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
+        database: process.env.NODE_ENV === "development" ? process.env.DB_NAME : process.env.TEST_DB_NAME,
         password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT
+        port: process.env.NODE_ENV === "development" ?  process.env.DB_PORT : process.env.TEST_DB_PORT
     })
     return pool;
 };
