1a11e9b86227e01eaefd7abedf4f7774abaf6b5d
emarkula24 <emarkula24@student.oulu.fi>
2024-11-26 23:46:56 +0200

    Created tests for sign up and sign in

--- server/helpers/test.js
+++ server/helpers/test.js
@@ -5,23 +5,43 @@ import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import { hash } from "bcrypt"
 import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config()
 const { sign } = jwt
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
-const initializeTestDb = () => {
+const initializeTestDb = async () => {
     const sql = fs.readFileSync(path.resolve(__dirname, "../test.sql"), "utf8");
-    pool.query(sql)
+    try {
+        await pool.query(sql)
+        
+    }catch (error) {
+        console.error("test db failed to initialize", error)
+        throw error
+    }
+    
 }
 
-const insertTestUser = (email, password) => {
-    hash(password, 10, (error, hashedPassword) => {
-        pool.query("insert into account (users_email, users_password) values ($1, $2)",
-            [email, hashedPassword])
-    })
+const insertTestUser = async (email, password) => {
+    try {     
+        const hashedPassword = await new Promise((resolve, reject) => {
+        hash(password, 10, (error, hashedPassword) => {
+            if (error) reject(error);
+            resolve(hashedPassword);
+        });
+    });
+        await pool.query("insert into users (users_email, users_password) values ($1, $2)", [email, hashedPassword])   
+        
+    } catch (error) {
+        console.error("Error inserting user")
+        throw error;
+    }
 }
+
 
 const getToken = (email) => {
     return sign({user: email}, process.env.JWT_SECRET_KEY)
--- server/index.test.js
+++ server/index.test.js
@@ -8,5 +8,91 @@ describe("POST login" , () => {
     before(() => {
         initializeTestDb()
     })
-    it ("should login user")
+
+    const email = "cremelog@gmail.com"
+    const password = "A1234567"
+    const wrongPassword = "notthepassword"
+    insertTestUser(email, password)
+
+    it ("should login with valid credentials", async() => {
+        const response = await fetch (base_url + "/user/login", {
+            method: "post",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({"users_email": email, "users_password": password})
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(200, data.error)
+        expect(data).to.be.an("object")
+        expect(data).to.include.all.keys("users_id", "users_email", "accessToken", "refreshToken")
+    })
+
+    
+
+    it ("should not login with invalid credentials", async () => {
+        const response = await fetch (base_url + "/user/login", {
+            method: "post",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({"users_email": email, "users_password": wrongPassword})
+        })
+        const data = await response.json()
+        
+        expect(response.status).to.equal(401, data.error)
+        expect(data).to.be.an("object")
+        expect(data).to.include.all.keys("error")
+    })
+})
+
+describe("POST register", () => {
+
+    it ("should register with valid email and password", async() => {
+        const email = "caramelreg@gmail.com"
+        const password = "A1234567"
+        const response = await fetch(base_url + "/user/register", {
+            method: "post",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({"users_email":email, "users_password": password})
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(201, data.error)
+        expect(data).to.be.an("object")
+        expect(data).to.include.all.keys("users_id", "users_email")
+    })
+
+    it ("should not register with less than 8 character password", async () => {
+        const email = "vanillareg@gmail.com"
+        const password = "short"
+        const response = await fetch(base_url + "/user/register", {
+            method: "post",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({"users_email":email, "users_password": password})
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(400, data.error)
+        expect(data).to.be.an("object")
+        expect(data).to.include.all.keys("error")
+    })
+
+    it ("should not register with a otherwise valid password that does not contain a capitalized letter", async () => {
+        const email = "cinnamonreg@gmail.com"
+        const password ="a1234567"
+        const response = await fetch(base_url + "/user/register", {
+            method: "post",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({"users_email":email, "users_password": password})
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(400, data.error)
+        expect(data).to.be.an("object")
+        expect(data).to.include.all.keys("error")
+    })
 })
