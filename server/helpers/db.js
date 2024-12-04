e00fdcac55c9770daf5aa063a3dfe27b6a7d8102
emarkula24 <emarkula24@student.oulu.fi>
2024-12-04 13:06:47 +0200

    changed yml

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -3,23 +3,54 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(`Connecting to database: ${process.env.NODE_ENV === "development" ? process.env.DB_NAME : process.env.TEST_DB_NAME}`);
-console.log(`Database Host: ${process.env.DB_HOST}`);
-console.log(`Database Port: ${process.env.NODE_ENV === "development" ? process.env.DB_PORT : process.env.TEST_DB_PORT}`);
+const isProduction = process.env.NODE_ENV === "production";
+if (isProduction) {
+    
+    console.log("Connecting to database:", process.env.DB_NAME)
+    console.log("Connecting to port:", process.env.DB_PORT)
+
+} else {
+
+    console.log(
+    `Connecting to database: ${
+      process.env.NODE_ENV === "development" ? process.env.DB_NAME : process.env.TEST_DB_NAME
+    }`
+  );
+
+  console.log(
+    `Database Port: ${
+      process.env.NODE_ENV === "development" ? process.env.DB_PORT : process.env.TEST_DB_PORT
+    }`
+  );
+  }
 
 const { Pool } = pkg;
 
-const openDb = () => {
-    const pool = new Pool ({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.NODE_ENV === "development" ? process.env.DB_NAME : process.env.TEST_DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.NODE_ENV === "development" ?  process.env.DB_PORT : process.env.TEST_DB_PORT
-    })
-    return pool;
-};
+let openDb;
+
+if (isProduction) {
+  openDb = () => {
+    return new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
+  };
+
+} else {
+  openDb = () => {
+    return new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.NODE_ENV === "development" ? process.env.DB_NAME : process.env.TEST_DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.NODE_ENV === "development" ? process.env.DB_PORT : process.env.TEST_DB_PORT,
+    });
+  };
+}
 
 const pool = openDb();
 
-export { pool }
+export { pool };
